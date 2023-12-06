353eed94ef19896a2a5b331e37e9ecc80527c160
Jordan <higgsfj@gmail.com>
2023-12-06 15:11:13 -0500

    Adding all files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import StarShipCard from './StarShipCard';
+
+
+function App() {
+    return <StarShipCard />
+}
+
+export default App
---
+++ src/StarShipCard.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+// https://swapi.dev/api/
+
+
+export default function StarShipCard() {
+
+    let [shipList, setShipList] = useState([]);
+
+    function showShip(ship, index) {
+        return (
+            <div key={index} className="ship">{ship.name}</div>
+        )
+    }
+
+
+
+    useEffect(() => {
+        async function getStarShips() {
+            let starShips = await fetch("https://swapi.dev/api/starships/?format=json")
+            let format = await starShips.json();
+            setShipList(format.results);
+        }
+        getStarShips();
+
+    }, [])
+
+
+    return (
+        <>
+            <header>Star Wars StarShips</header>
+            <div className="ship-holder">
+                {shipList.map(showShip)}
+            </div>
+        </>
+    )
+
+
+}
